091df3c259795c38f9cfa2145055bb8d1eecd59f
ayushisingh-08 <aayushisingh0105@gmail.com>
2025-07-11 00:02:22 +0530

    Add files via upload
    Add palindrome checker and simple calculator programs.

---
+++ PalindromeChecker.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        if(original.equalsIgnoreCase(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
+}
